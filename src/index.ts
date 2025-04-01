2208b5e7b934a1236d8808f16bda3263657fc8cd
Nathan Schrader <natecschrader@gmail.com>
2025-04-01 10:50:39 -0700

    add test logs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { LogProcessor } from './LogProcessor';
 
 const logProcessor = new LogProcessor();
 
-logProcessor.addLog('First log');
-logProcessor.addLog('Second log');
+logProcessor.addLog({
+  message:
+    'I last replaced my roof at my 64th Street property on April 2nd, 2020.',
+  level: 'info',
+  property_description: '64th street',
+});
 logProcessor.processLogs();
